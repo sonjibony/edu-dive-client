65f8b552302d4cb480d7486b62bfe0221812bc7c
sonjibony <sonjibony@gmail.com>
2022-10-25 00:41:41 +0600

    navbar done

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,9 +1,30 @@
 import { createBrowserRouter } from "react-router-dom";
+import Blog from "../../components/Blog/Blog";
+import Courses from "../../components/Courses/Courses";
+import Home from "../../components/Home/Home";
 import Main from "../../layout/Main";
 
 export const routes = createBrowserRouter([
     {
         path:'/',
-        element: <Main></Main>
+        element: <Main></Main>,
+        children:[
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/courses',
+                element: <Courses></Courses>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            },
+            {
+                path: '/*',
+                element: <div>sorry</div>
+            },
+        ]
     }
 ])
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,43 @@
 import React from 'react';
+// importing components from react bootstrap
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-           header 
-        </div>
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light" >
+      <Container>
+        <Navbar.Brand href="#home">
+        <img
+              alt=""
+              src="https://cdn-icons-png.flaticon.com/512/2729/2729012.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            /> {''}
+            Edu Dive
+            </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link> <Link  className='text-decoration-none text-dark'  to='/'>Home</Link> </Nav.Link>
+            <Nav.Link> <Link className='text-decoration-none text-dark' to='/courses'>Courses</Link> </Nav.Link>
+            <Nav.Link> <Link className='text-decoration-none text-dark' to='/blog'>Blog</Link></Nav.Link>
+            <Nav.Link><Link className='text-decoration-none text-dark' to='/faq'>FAQ</Link></Nav.Link>
+            
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
     );
 };
 
