1b63e21dd4e20a6d7bccd4bbf433b78fdb902acf
sonjibony <sonjibony@gmail.com>
2022-10-27 11:43:49 +0600

    footer done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaSignInAlt } from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        <div style={{backgroundColor:"#050e38", color:"white", padding:"40px"}}>
-            footer
+        <div>
+        <div className='d-lg-flex align-items-center justify-content-evenly gap-4 text-start p-5' style={{backgroundColor:"#050e38", color:"white", padding:"40px",}}>
+            <div style={{width:"300px"}}>
+            <div className='d-flex align-items-center gap-2 ' >
+               <img style={{height:"70px"}} src="https://cdn-icons-png.flaticon.com/512/3721/3721186.png" alt=""></img>
+               <h3 className='text-white'>Edu Hive</h3>
+            </div>
+            <h6 className='mt-3 text-start text-white'>Edu Hive is a wellknown online based learning platform. We are in business for 4 years now.</h6>
+            </div>
+            <div >
+<h3 className='text-white' >Our Link</h3>
+<h6  > <Link className='text-white' to='/'>Home</Link> </h6>
+<h6  > <Link className='text-white' to='/courses'>Courses</Link> </h6>
+<h6> <Link className='text-white' to='/blog'>Blog</Link> </h6>
+<h6 className='text-white'> Contact Us </h6>
+<h6 className='text-white'> About Us </h6>
+            </div>
+            <div>
+            <h3 className='text-white'>Other Link</h3>
+<h6 > <Link className='text-white' to='/faq'>FAQ</Link> </h6>
+<h6 className='text-white'>Instructors</h6>
+<h6 className='text-white'>Event </h6>
+<h6 className='text-white'>Privacy Policy</h6>
+<h6 className='text-white' >Terms & Conditions</h6>
+            </div>
+            <div>
+                <h3 className='text-white'>
+                    Subscribe Now
+                </h3>
+                <button className='btn' style={{backgroundColor:"#fd5e2a"}}><FaSignInAlt className='text-white '></FaSignInAlt></button>
+                <input className='pt-2 pb-3 ps-3 ms-2 rounded border-0' type="email" name="email" id="" placeholder='Email Address'  />
+
+            </div>
+            </div>
+            <div className=' text-center p-3' style={{backgroundColor:"#020827"}}>
+                <h6 className='text-white'>Copyright © 2022 <span style={{color:"#fd5e2a"}}>Sonjibony Wahid. </span>  All rights reserved.
+ </h6>
+            </div>
         </div>
     );
 };
-
+ 
 export default Footer;
--- src/components/Missions/Missions.js
+++ src/components/Missions/Missions.js
@@ -6,8 +6,8 @@ const Missions = () => {
           
           <div className='d-flex flex-sm-column flex-lg-row align-items-center mt-5 mb-5 text-light text-start ' style={{backgroundColor:"#050e38"}}>
             < div className='p-5 w-75  '>
-            <h5>CORE FEATURES</h5>
-          <h1>See What Our Missions Are</h1>
+            <h5 className='text-white'>CORE FEATURES</h5>
+          <h1  style={{color:"#fd5e2a"}} >See What Our Missions Are</h1>
           <div className='d-flex align-items-center gap-3 mb-4 mt-4'>
             <div>
             <img src="https://cdn-icons-png.flaticon.com/512/327/327131.png" alt=""  style={{height:"70px"}}></img>
@@ -15,7 +15,7 @@ const Missions = () => {
 
             </div>
             <div>
-            <h5>Student Life</h5>
+            <h5 className='text-white'>Student Life</h5>
             <p>This course will help you in your student life specially if your major in computer science.</p> 
             </div>
             
@@ -26,7 +26,7 @@ const Missions = () => {
  
             </div>
             <div>
-            <h5>24x7 Program</h5>
+            <h5 className='text-white'>24x7 Program</h5>
             <p>You will find us giving services 24 hours. Our mentors will always be there for you.</p> 
             </div>
           </div>
@@ -35,7 +35,7 @@ const Missions = () => {
                 <img src="https://cdn-icons-png.flaticon.com/512/3195/3195496.png" alt=""  style={{height:"70px"}}></img>
             </div>
             <div>
-            <h5>Best Online Class</h5>
+            <h5 className='text-white'> Best Online Class</h5>
             <p>We can assure you that our courses will not disappoint you. We offer the best we can give. </p> 
             </div>
           </div>
